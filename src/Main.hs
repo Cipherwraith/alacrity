1e78c3fa300d74d03582f18786d7ed1200662909
Cipherwraith <cipherwraith666@gmail.com>
2016-03-02 20:35:54 +0900

    added example for arg passing

--- src/Main.hs
+++ src/Main.hs
@@ -14,6 +14,10 @@ import qualified Network.WebSockets as WS
 import System.IO
 
 -- Todo:
+--    example ->
+--      `./alacrity -p1001 -r/srv/http -d10 -t250000`
+--      port 1001, root directory is /srv/http, 10 ticks to cache death, and 250 milliseconds per tick
+--
 --   - Options passed on startup -
 --     * Make a way to define root path
 --     * timeToDie for pages
