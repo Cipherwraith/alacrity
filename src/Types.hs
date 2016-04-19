9977c2402619af9b0ab7ce45b7919d044b15443f
Cipherwraith <cipherwraith666@gmail.com>
2016-04-19 14:39:12 +0900

    unpacked and forced strictness on types

--- src/Types.hs
+++ src/Types.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE MagicHash #-}
 module Types where
 import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString as B hiding (unpack, pack)
@@ -21,15 +22,15 @@ type ServerState = TVar State
 
 -- Data about our page stored in memory
 data Page = Page { 
-    _pagePath   :: FilePath -- Path where the file is stored
-  , _pageData   :: FileData -- Data stored in the file
-  , _timeToDie  :: Int      -- Seconds until the data is removed from State cache
-  , _onHarddisk :: Bool     -- Whether or not this page has been saved to hard disk
+    _pagePath    :: {-# UNPACK #-} !(FilePath) -- Path where the file is stored
+  , _pageData    :: {-# UNPACK #-} !(FileData) -- Data stored in the file
+  , _timeToDie   :: {-# UNPACK #-} !(Int)      -- Seconds until the data is removed from State cache
+  , _onHarddisk  :: {-# UNPACK #-} !(Bool)     -- Whether or not this page has been saved to hard disk
   } deriving (Show)
 
 -- Cache pages in the server state until their _timeToDie hits 0
 data State = State {
-    _pages :: HM.HashMap FilePath Page
+    _pages :: !(HM.HashMap FilePath Page)
   } deriving (Show)
 
 -- Input data
@@ -37,30 +38,35 @@ type FileData = B.ByteString
 type ErrorText = String
 
 -- Server Output
-data ServerOut = ErrorMsg String | DataSaved String | ViewData FilePath B.ByteString | ViewRawData FilePath B.ByteString
+data ServerOut = ErrorMsg !(String) 
+               | DataSaved !(String) 
+               | ViewData !(FilePath) !(B.ByteString)
+               | ViewRawData !(FilePath) !(B.ByteString)
 
 data Rawness = Raw | WellDone
 
 instance ToJSON ServerOut where
-  toJSON (ErrorMsg  errorMessage) = 
+  toJSON !(ErrorMsg  errorMessage) = 
     object ["error" .= errorMessage]
 
-  toJSON (DataSaved fileName) = 
+  toJSON !(DataSaved fileName) = 
     object ["saved" .= fileName]
 
-  toJSON (ViewData fileName contents) = 
+  toJSON !(ViewData fileName contents) = 
     object ["view" .= (binaryToBase64 contents), "file" .= fileName]
 
-  toJSON (ViewRawData _ contents) = String . binaryToBase64 $ contents
+  toJSON (ViewRawData _ !(contents)) = String $! binaryToBase64 contents
 
 binaryToBase64 :: B.ByteString -> T.Text
-binaryToBase64 b = T.pack . B.unpack . B64.encode $ b
+binaryToBase64 !b = T.pack . B.unpack . B64.encode $! b
 
 base64ToBinary :: T.Text -> B.ByteString
-base64ToBinary t = B64.decodeLenient . B.pack . T.unpack $ t
+base64ToBinary !t = B64.decodeLenient . B.pack . T.unpack $! t
 
 -- Command list
-data Command = Store FilePath B.ByteString | View FilePath | ViewRaw FilePath 
+data Command = Store !(FilePath) !(B.ByteString)
+             | View !(FilePath)
+             | ViewRaw !(FilePath)
 
 -- Settings for index assumption
 --   if _assumeIndex is true, then we will assume that 
@@ -69,15 +75,15 @@ data Command = Store FilePath B.ByteString | View FilePath | ViewRaw FilePath
 --   If _assumeIndex is false, then we wont assume anything. 
 --     note: server will return an error if just a path is requested
 data IndexSettings = IndexSettings {
-    _assumeIndex     :: Bool
-  , _indexAssumption :: FilePath
+    _assumeIndex     :: {-# UNPACK #-} !(Bool)
+  , _indexAssumption :: {-# UNPACK #-} !(FilePath)
 } deriving (Show)
 
 data Msg = Msg {
-    _command :: String
-  , _password :: Maybe String
-  , _filepath :: FilePath
-  , _filedata :: Maybe T.Text
+    _command :: {-# UNPACK #-} !(String)
+  , _password :: {-# UNPACK #-} !(Maybe String)
+  , _filepath :: {-# UNPACK #-} !(FilePath)
+  , _filedata :: {-# UNPACK #-} !(Maybe T.Text)
 }
 
 -- Support for mime types
@@ -87,12 +93,12 @@ data CustomOut = PDF | OGG | MP4 | SVG | ICO | PNG | GIF | WEBM | HTML | JSON | 
 data FileEncoding = UTF8 | Binary deriving (Eq, Ord)
 
 data PreppedFile = PreppedFile {
-    _getText   :: Maybe TL.Text
-  , _getBinary :: Maybe BL.ByteString
+    _getText   :: {-# UNPACK #-} !(Maybe TL.Text)
+  , _getBinary :: {-# UNPACK #-} !(Maybe BL.ByteString)
 } 
 
 instance FromJSON Msg where
-  parseJSON (Object v) = 
+  parseJSON !(Object v) = 
     Msg <$>
     (v .:  "command")  <*>
     (v .:? "password") <*>
